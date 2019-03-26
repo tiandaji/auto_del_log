8ab40c4c876b1d179110708aff85fa4ea81e77d0
tiandaji <35362216+tiandaji@users.noreply.github.com>
2019-03-26 16:15:38 +0800

    Update auto_del_log.sh

--- auto_del_log.sh
+++ auto_del_log.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-find /usr/local/shadowsocksr/ -mtime +10 -name "*.log" -exec rm -rf {} \;
+#find /usr/local/shadowsocksr/ -mtime +10 -name "*.log" -exec rm -rf {} \;
+find /usr/local/shadowsocksr/ -name "*.log" -exec rm -rf {} \;
 PID=`ps -ef |grep -v grep | grep server.py |awk '{print $2}'`
 [[ ! -z ${PID} ]] && /etc/init.d/ssrmu stop
 /etc/init.d/ssrmu start
